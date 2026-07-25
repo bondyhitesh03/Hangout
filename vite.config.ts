ff96b3b689706347c93f7e2bbe29ae8aac77e793
Hitesh <bondyhitesh03@gmail.com>
2026-07-25 21:30:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Hangout/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
